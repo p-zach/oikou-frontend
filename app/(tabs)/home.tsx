69759d93b5302c148d439b3f71fa1af999ab8ab2
p-zach <porterdzach@gmail.com>
2025-12-19 21:13:30 -0500

    Add nativewind

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,10 @@
+import "@/global.css";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl font-bold text-blue-500">Home</Text>
     </View>
   );
 }
