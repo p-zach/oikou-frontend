3a078200465b71909c9eedd7632c7b1b44cdffdc
p-zach <porterdzach@gmail.com>
2026-02-22 12:00:52 -0500

    Fix scroll not working on tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,19 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { TabList, Tabs, TabSlot, TabTrigger } from "expo-router/ui";
+import { Link, Stack } from "expo-router";
 import { View } from 'react-native';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <TabSlot />
-      <TabList className='bg-background border-border border-t flex-row justify-around'>
-        <TabTrigger name='home' href={'/home'} className='p-2 flex-grow justify-center'>
-          <View >
-            <Ionicons name="home" size={36} className='text-textPrimary' />
-          </View>
-        </TabTrigger>
-        <TabTrigger name='profile' href={'/profile'} className='p-2 flex-grow justify-center'>
-          <View>
-            <Ionicons name="person-circle" size={36} className='text-textPrimary' />
-          </View>
-        </TabTrigger>
-      </TabList>
-    </Tabs>
+    <View className='flex-1'>
+      <Stack screenOptions={{ headerShown: false }} />
+      <View className='bg-background border-border border-t flex-row justify-around'>
+        <Link href={'/home'} className='p-2 flex-grow flex flex-row justify-center'>
+          <Ionicons name="home" size={36} className='text-textPrimary' />
+        </Link>
+        <Link href={'/profile'} className='p-2 flex-grow flex flex-row justify-center'>
+          <Ionicons name="person-circle" size={36} className='text-textPrimary' />
+        </Link>
+      </View>
+    </View>
   );
 }
