b57de37ce7932a54c6e0f8da379cbb9bb6079238
p-zach <porterdzach@gmail.com>
2026-03-10 18:44:46 -0400

    Add Vercel analytics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import AppWrapper from '@/components/app-wrapper';
 import { ThemeProvider } from '@/components/theme-provider';
 import '@/global.css';
+import { inject } from "@vercel/analytics";
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import Head from 'expo-router/head';
 import { useEffect } from 'react';
+import { Platform } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -12,6 +14,12 @@ export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     'Lexend-Regular': require('../assets/fonts/lexend/static/Lexend-Regular.ttf'),
   });
+
+  // Inject analytics on web
+  useEffect(() => {
+    if (Platform.OS === "web")
+      inject();
+  }, [])
 
   useEffect(() => {
     if (fontsLoaded) {
