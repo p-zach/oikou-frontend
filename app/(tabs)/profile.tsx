b750713dfaf208a87a493e6e145647e5de35af83
p-zach <porterdzach@gmail.com>
2026-03-10 18:12:51 -0400

    Add UUID explainer text

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -20,8 +20,13 @@ export default function Profile() {
         <AppText className="text-4xl">Profile</AppText>
         <AppText className="text-2xl">Theme Preference</AppText>
         <ThemeToggle />
-        <AppText className="text-2xl">Local UUID</AppText>
-        <AppText>{localUUID}</AppText>
+        <AppText className="text-2xl">Local User ID</AppText>
+        <AppText className="-mt-4 text-md">{localUUID}</AppText>
+        <AppText className="-mt-4 text-md italic">
+          This value is stored in your browser cache and serves as your unique user identifier.
+          This allows your progress to be stored until accounts are implemented.
+          Clearing your browser cache will reset your progress!
+        </AppText>
       </View>
     </ScrollView>
   );
